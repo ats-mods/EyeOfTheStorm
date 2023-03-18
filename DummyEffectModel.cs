8f2bc021d30af7a780cf23e495498c8edcfadaa6
Sporadic <matty_119@hotmail.com>
2023-03-18 13:20:19 +0100

    dummy effect model

---
+++ DummyEffectModel.cs
@@ -0,0 +1,38 @@
+using Eremite.Buildings;
+using Eremite.Model;
+using Eremite.Model.Effects;
+using UnityEngine;
+
+namespace EyeOfTheStorm
+{
+    public class DummyEffectModel : EffectModel {
+        private float amount = 0f;
+
+        public override string GetAmountText()
+        {
+           return this.amount.RoundToInt().ToString();
+        }
+
+        public override void OnApply(EffectContextType contextType, int contextId)
+        {
+        }
+
+        public override bool HasImpactOn(BuildingModel building)
+        {
+            return false;
+        }
+
+        public override bool IsPerk => true;
+
+        public override Sprite GetDefaultIcon()
+        {
+           return this.overrideIcon;
+        }
+
+        public override Color GetTypeColor()
+        {
+           return base.Settings.RewardColorCommonNegative;
+        }
+
+    }
+}
