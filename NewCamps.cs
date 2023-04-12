6ab57d9252121d935c971df3ea16008037d081ab
Sporadic <matty_119@hotmail.com>
2023-04-12 08:45:51 +0200

    Also primitive harvester

--- NewCamps.cs
+++ NewCamps.cs
@@ -17,7 +17,9 @@ namespace EyeOfTheStorm
 
         public static void Patch(){
             var primitiveStonecutter = CloneModel("Stonecutter's Camp");
+            var primitiveHarvester = CloneModel("Harvester Camp");
             AddSOsToSettings(primitiveStonecutter);
+            AddSOsToSettings(primitiveHarvester);
             Done();
         }
 
@@ -38,6 +40,9 @@ namespace EyeOfTheStorm
         private static void ClonePrefab(GathererHutModel model){
             var prefab = (GathererHut) model.prefab.Clone();
             model.prefab = prefab;
+            // Strangely, the model field for all Gatherer huts is set to the same stonecutter camp model
+            // Which leads me to believe this field is unused. Thus, setting it is unneeded
+            // prefab.model = model;
         }
 
         private static GathererHutModel PrimitiveModel => (GathererHutModel) Settings.GetBuilding("Primitive Trapper's Camp");
@@ -64,8 +69,23 @@ namespace EyeOfTheStorm
         public static void UpdateEssentialBuildings(){
             var content = Serviceable.GameContentService;
             content.Lock(Settings.GetBuilding("Stonecutter's Camp"));
+            content.Lock(Settings.GetBuilding("Harvester Camp"));
             content.Unlock(Settings.GetBuilding("Primitive Stonecutter's Camp"));
+            content.Unlock(Settings.GetBuilding("Primitive Harvester Camp"));
+        }
+
+        public static void DumpBlueprints(int index){ // For debug purposes only
+            var blueprints = Settings.blueprintsConfigs[index];
+            Plugin.Log($"Dumping information for {blueprints.Name}");
+            foreach(var bp in blueprints.blueprints){
+                Plugin.Log($"BlueprintRange {bp.range.x}-{bp.range.y}: has sets:");
+                foreach(var set in bp.sets){
+                    Plugin.Log($"  {set.Name} ({set.weight}), buildings:");
+                    foreach(var building in set.buildings){
+                        Plugin.Log($"    {building.building.Name} ({building.weight})");
+                    }
+                }
+            }
         }
     }
-
 }
